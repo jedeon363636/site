121c5e4fa9028cb04cf3ef7370e4a5358d64ec42
jedeon <jedeondev@protonmail.com>
2021-05-05 10:35:41 +0200

    ajout fichier statique sur l app blog

--- src/Project/urls.py
+++ src/Project/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+from blog.views import index
 from Project.views import accueilView
 
 urlpatterns = [
     path('',accueilView),
+    path('blog/',index),
     path('admin/', admin.site.urls),
 ]
